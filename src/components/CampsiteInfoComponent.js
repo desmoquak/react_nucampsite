35b3bece2f49b51f56e86bcae059a1848ef4aa3a
John Schaub <schaubjs@gmail.com>
2021-03-15 21:10:17 -0600

    React About Page

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,35 +1,43 @@
 import React from 'react';
-import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardText,
+  Breadcrumb,
+  BreadcrumbItem,
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-function RenderComments({comments}) {
+function RenderComments({ comments }) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
         <h4>Comments</h4>
         {comments.map((c) => (
           <p key={c.id}>
-            {c.text} <br />-- {c.author},
+            {c.text} <br />
+            -- {c.author},
             {new Intl.DateTimeFormat('en-US', {
               year: 'numeric',
               month: 'short',
               day: '2-digit',
             }).format(new Date(Date.parse(c.date)))}
           </p>
-        
         ))}
-      </div> 
+      </div>
     );
   }
   return <div />;
 }
 
-function RenderCampsite({campsite}) {
+function RenderCampsite({ campsite }) {
   return (
     <div className="col-md-5 m-1">
       <Card>
         <CardImg top src={campsite.image} alt={campsite.name} />
         <CardBody>
-          <CardTitle>{campsite.name}</CardTitle>
           <CardText>{campsite.description}</CardText>
         </CardBody>
       </Card>
@@ -42,8 +50,20 @@ function CampsiteInfo(props) {
     return (
       <div className="container">
         <div className="row">
-          <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.campsite.comments} />
+          <div className="col">
+            <Breadcrumb>
+              <BreadcrumbItem>
+                <Link to="/directory">Directory</Link>
+              </BreadcrumbItem>
+              <BreadcrumbItem active>{}</BreadcrumbItem>
+            </Breadcrumb>
+            <h2>{props.campsite.name}</h2>
+            <hr />
+          </div>
+          <div className="row">
+            <RenderCampsite campsite={props.campsite} />
+            <RenderComments comments={props.comments} />
+          </div>
         </div>
       </div>
     );
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 // Render each component with campsite details
 function RenderDirectoryItem({campsite}) {
   return (
     <Card>
-      <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-      <CardImgOverlay>
-        <CardTitle>{campsite.name}</CardTitle>
-      </CardImgOverlay>
+        <Link to={`/directory/${campsite.id}`}>
+                <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
     </Card>
+    
   );
 }
 
@@ -17,16 +21,27 @@ function Directory(props) {
   
   const directory = props.campsites.map((campsite) => {
     return (
-      <div key={campsite.id} className=" col-md-5 m-1 ">
-         <RenderDirectoryItem campsite={campsite} />
-      </div>
+        <div key={campsite.id} className=" col-md-5 m-1 ">
+            <RenderDirectoryItem campsite={campsite} />
+        </div>
     );
   });
 
   return (
-    <div className="container">
-      <div className="row">{directory}</div>
-    </div>
+      <div className="container">
+            <div className='row'>
+                <div className='col'>
+                      <Breadcrumb>
+                            <BreadcrumbItem>
+                            <Link to ="/home">Home</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>Directory</BreadcrumbItem>
+                      </Breadcrumb>
+                      <h2>directory</h2>
+                      <hr />
+                </div>
+            </div>
+         <div className="row">{directory}</div>
+      </div>
   );
 }
 
