c6226016f5fc59e005eceaef1ac08ccb82f5d3f6
John Schaub <schaubjs@gmail.com>
2021-03-18 20:48:43 -0600

    Uncontrolled Form

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import 'typeface-lobster';
 import 'typeface-open-sans';
 import reportWebVitals from './reportWebVitals';
