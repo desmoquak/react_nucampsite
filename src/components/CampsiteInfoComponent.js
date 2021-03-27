597d62e80dec3042e4f429e5ea5bcdf6bb19ccac
John Schaub <schaubjs@gmail.com>
2021-03-26 19:42:15 -0600

    React Transitions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -38,7 +38,7 @@ class CommentForm extends Component {
   }
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(
+    this.props.postComment(
       this.props.campsiteId,
       values.rating,
       values.author,
@@ -131,7 +131,7 @@ class CommentForm extends Component {
   }
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -147,7 +147,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
             }).format(new Date(Date.parse(c.date)))}
           </p>
         ))}
-        <CommentForm campsiteId={campsiteId} addComment={addComment} />
+        <CommentForm campsiteId={campsiteId} postComment={postComment} />
       </div>
     );
   }
@@ -208,7 +208,7 @@ function CampsiteInfo(props) {
             <RenderCampsite campsite={props.campsite} />
             <RenderComments
               comments={props.comments}
-              addComment={props.addComment}
+              postComment={props.postComment}
               campsiteId={props.campsite.id}
             />
           </div>
